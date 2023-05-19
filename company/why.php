208065b098049314f3cce567ffe4a6fce9640581
Krishna00792 <sai017krishna@gmail.com>
2023-05-19 21:29:13 +0530

    s

--- company/why.php
+++ company/why.php
@@ -2,33 +2,72 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Xcutives</title>
-  <?php include('../header-links.php') ?>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Xcutives</title>
+    <?php include('../header-links.php') ?>
 </head>
 
 <body>
-  <!-- header  -->
-<?php include('../header.php') ?>
-  <!-- header  -->
+    <!-- header  -->
+    <?php include('../header.php') ?>
+    <!-- header  -->
+    <section class="container my-5">
+        <div class="row">
+            <div class="col-6 text-center">
+                <!-- Image -->
+                <img src="../img/why.png" class="img-fluid" alt="Image description">
+            </div>
+            <div class="row-lg-6">
+                <h2>Consulting Excellence Redefined</h2>
+                <p>
+
+At our core, we are dedicated to delivering unparalleled consulting services that continuously exceed expectations. Through our systematic benchmarking solutions, we empower you with a profound understanding of your unique requirements, backed by the invaluable insights gathered by our experienced team throughout years of industry expertise.
+
+We firmly believe in customized solutions that are tailor-made to fit your specific needs and realities. In today's ever-evolving world of perpetual change, true leaders go beyond mere adaptation; they anticipate and seize new opportunities with agility. With our expertise and talented professionals, we are committed to helping you bring your ideas to life and develop innovative solutions. We take the time to listen attentively, understanding your organizational culture, and become your trusted partner in overcoming your business challenges.
+
+We specialize in fostering agility and momentum, providing meticulous planning, flawless implementation, and real-world application of our benchmarking solutions. Together, we can propel your organization forward, embracing change, and seizing every opportunity for growth.</p>
+                
+            </div>
+            <div class="text-card">
+                <h2>Outmatch the Pace:</h2>
+
+<p> We surpass the competition in speed and precision, consistently placing exceptional talent at an unparalleled pace.</p>
+
+<h2>Make it Possible:</h2>
+
+<p> We are driven by impactful insights, expertise that outsmarts the most formidable challenges, agility that sparks new acceleration, and a collaborative approach that simplifies diverse ideas.</p>
+
+<h2>Diversity, Equity, and Inclusion (DEI) by Design, Not by Default:</h2>
+
+<p>While companies worldwide strive to establish a foothold in the DEI space, we are already at the forefront, offering solutions that enable organizations to foster inclusivity.</p>
+
+<h2>We Conduct Business the Right Way:</h2>
+
+<p>Our Code of Conduct embodies our values, providing guidance and upholding the highest ethical standards for our people.</p>
+
+<h2>We Care:</h2>
+
+<p>Both our team and our clients are at the forefront of building a future that benefits everyone. We wholeheartedly support and celebrate one another, promoting personal and professional growth, fostering connections, and inspiring achievements in all their forms.</p>
+            </div>
+        </div>
+    </section>
+
+
+    <!-- call-back -->
+    <?php include('../call-back.php') ?>
+    <!-- call-back -->
+
+    <!-- Footer -->
+    <?php include('../footer.php') ?>
+    <!-- Footer -->
+
+    <!-- script   -->
 
 
 
-  <!-- call-back -->
-  <?php include('../call-back.php') ?>
-  <!-- call-back -->
-
-  <!-- Footer -->
-  <?php include('../footer.php') ?>
-  <!-- Footer -->
-
-<!-- script   -->
-
-  
-
-<!-- script  -->
+    <!-- script  -->
 </body>
 
 </html>
