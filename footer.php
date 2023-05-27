f136acbb5ea55eca7beb887bd6877a48e47f7d00
Krishna00792 <sai017krishna@gmail.com>
2023-05-27 20:11:46 +0530

    new

--- footer.php
+++ footer.php
@@ -1,113 +1,125 @@
-
 <footer class="text-center text-lg-start bg-light text-muted">
-  <!-- Section: Social media -->
-  <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
-    <!-- Left -->
-    <div class="me-5 d-none d-lg-block">
-      <span>Get connected with us on social networks:</span>
-    </div>
-    <!-- Left -->
-
-    <!-- Right -->
-    <div>
-      <a href="" class="me-4 text-reset">
-        <i class="bi bi-facebook"></i>
-      </a>
-      <a href="" class="me-4 text-reset">
-        <i class="bi bi-twitter"></i>
-      </a>
-      <a href="" class="me-4 text-reset">
-        <i class="bi bi-google"></i>
-      </a>
-      <a href="" class="me-4 text-reset">
-        <i class="bi bi-instagram"></i>
-      </a>
-      <a href="" class="me-4 text-reset">
-        <i class="bi bi-linkedin"></i>
-      </a>
-    </div>
-    <!-- Right -->
-  </section>
-  <!-- Section: Social media -->
-
-  <!-- Section: Links  -->
-  <section class="">
-    <div class="container text-center text-md-start mt-5">
-      <!-- Grid row -->
-      <div class="row mt-3">
-        <!-- Grid column -->
-        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-          <!-- Content -->
-          <h6 class="text-uppercase fw-bold mb-4">
-            <i class="fas fa-gem me-3"></i>Company name
-          </h6>
-          <p>
-            Here you can use rows and columns to organize your footer content. Lorem ipsum
-            dolor sit amet, consectetur adipisicing elit.
-          </p>
-          <p>© Copyrights. All rights reserved. Bootstrapious.com</p>
+    <!-- Section: Social media -->
+    <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+        <!-- Left -->
+        <div class="me-5 d-none d-lg-block">
+            <span>Get connected with us on social networks:</span>
         </div>
-        <!-- Grid column -->
+        <!-- Left -->
 
-        <!-- Grid column -->
-        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-          <!-- Links -->
-          <h6 class="text-uppercase fw-bold mb-4">
-            Products
-          </h6>
-          <p>
-            <a href="#!" class="text-reset">Angular</a>
-          </p>
-          <p>
-            <a href="#!" class="text-reset">React</a>
-          </p>
-          <p>
-            <a href="#!" class="text-reset">Vue</a>
-          </p>
-          <p>
-            <a href="#!" class="text-reset">Laravel</a>
-          </p>
+        <!-- Right -->
+        <div>
+            <a href="" class="me-4 text-reset">
+                <i class="bi bi-facebook"></i>
+            </a>
+            <a href="" class="me-4 text-reset">
+                <i class="bi bi-twitter"></i>
+            </a>
+            <a href="" class="me-4 text-reset">
+                <i class="bi bi-google"></i>
+            </a>
+            <a href="" class="me-4 text-reset">
+                <i class="bi bi-instagram"></i>
+            </a>
+            <a href="" class="me-4 text-reset">
+                <i class="bi bi-linkedin"></i>
+            </a>
         </div>
-        <!-- Grid column -->
+        <!-- Right -->
+    </section>
+    <!-- Section: Social media -->
 
-        <!-- Grid column -->
-        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-          <!-- Links -->
-          <h6 class="text-uppercase fw-bold mb-4">
-            Useful links
-          </h6>
-          <p>
-            <a href="#!" class="text-reset">Pricing</a>
-          </p>
-          <p>
-            <a href="#!" class="text-reset">Settings</a>
-          </p>
-          <p>
-            <a href="#!" class="text-reset">Orders</a>
-          </p>
-          <p>
-            <a href="#!" class="text-reset">Help</a>
-          </p>
+    <!-- Section: Links  -->
+    <section class="">
+        <div class="container text-center text-md-start mt-5">
+            <!-- Grid row -->
+            <div class="row mt-3">
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                    <!-- Content -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        <i class="fas fa-gem me-3"></i>Company name
+                    </h6>
+                    <p>
+                        Here you can use rows and columns to organize your footer content. Lorem ipsum
+                        dolor sit amet, consectetur adipisicing elit.
+                    </p>
+                    <p>© Copyrights. All rights reserved. Bootstrapious.com</p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        Products
+                    </h6>
+                    <p>
+                        <a href="#!" class="text-reset">Angular</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">React</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Vue</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Laravel</a>
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                    <!-- Links -->
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        Useful links
+                    </h6>
+                    <p>
+                        <a href="#!" class="text-reset">About Us</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Why XYZ
+                        </a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Our Team
+                        </a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Testimonials
+                        </a>
+                    </p>
+                    <p>
+                        <a href="" class="text-reset">Become a Client
+                        </a>
+                    </p>
+                    <p>
+                        <a href="" class="text-reset">Our Clients</a>
+                    </p>
+                </div>
+                <!-- Grid column -->
+
+                <!-- Grid column -->
+                <div class="col-lg-4 col-md-6">
+                    <h6 class="text-uppercase fw-bold mb-4">Newsletter.</h6>
+                    <p class="small text-muted">Subscribing to our mailing list and receive weekly newsletter with
+                        latest news and offers.</p>
+                    <form action="#">
+                        <div class="input-group mb-3">
+                            <input class="form-control" type="text" placeholder="Recipient's username"
+                                aria-label="Recipient's username" aria-describedby="button-addon2">
+                            <button class="btn btn-primary" id="button-addon2" type="button"><i
+                                    class="bi bi-send-fill"></i></button>
+                        </div>
+                    </form>
+                </div>
+                <!-- Grid column -->
+            </div>
+            <!-- Grid row -->
         </div>
-        <!-- Grid column -->
+    </section>
+    <!-- Section: Links  -->
 
-        <!-- Grid column -->
-        <div class="col-lg-4 col-md-6">
-          <h6 class="text-uppercase fw-bold mb-4">Newsletter.</h6>
-          <p class="small text-muted">Subscribing to our mailing list and receive weekly newsletter with latest news and offers.</p>
-          <form action="#">
-              <div class="input-group mb-3">
-                  <input class="form-control" type="text" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
-                  <button class="btn btn-primary" id="button-addon2" type="button"><i class="bi bi-send-fill"></i></button>
-              </div>
-          </form>
-      </div>
-        <!-- Grid column -->
-      </div>
-      <!-- Grid row -->
-    </div>
-  </section>
-  <!-- Section: Links  -->
 
- 
 </footer>
